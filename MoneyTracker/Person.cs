6f584796f3d3be0a443776bacfd59506c4265f28
Dario Dabovic <dabovic.d02@htlwienwest.at>
2021-01-12 19:27:36 +0100

    Fertig

---
+++ MoneyTracker/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneyTracker
+{
+    class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public Person(string firstname, string lastname)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+    }
+}
