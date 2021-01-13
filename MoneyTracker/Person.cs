f959cf17fbf2ad4008c9263fd0d82036cbcac4f6
Dominik Hofmann <hofmann.d04@htlwienwest.at>
2021-01-13 13:53:03 +0100

    public into internal

--- MoneyTracker/Person.cs
+++ MoneyTracker/Person.cs
@@ -6,8 +6,8 @@ namespace MoneyTracker
 {
     class Person
     {
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
+        internal string Firstname { get; set; }
+        internal string Lastname { get; set; }
 
         public Person(string firstname1, string lastname1)
         {
